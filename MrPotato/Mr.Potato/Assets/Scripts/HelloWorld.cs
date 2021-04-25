475ff677967f90031834bcaea1b2eb5ba858fd28
Xin7K <350943239@qq.com>
2021-04-25 19:43:59 +0800

    完成场景搭建，人物组装

---
+++ MrPotato/Mr.Potato/Assets/Scripts/HelloWorld.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+//MonoBehaviour Unity大的父类
+/**
+生命周期
+初始化函数： Awake -> Start
+更新函数： FixedUpdate -> Update
+销毁函数： Destory
+*/
+public class HelloWorld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void Awake()
+    {
+        Debug.Log("Awake");
+    }
+
+}
