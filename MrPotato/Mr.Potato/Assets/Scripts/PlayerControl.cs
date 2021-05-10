d843f9572614082d878ec8c2e8f6e3834c01d197
Xin7K <350943239@qq.com>
2021-05-10 18:02:31 +0800

    实现转身跳跃

---
+++ MrPotato/Mr.Potato/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    private Rigidbody2D heroBody;
+
+    public float MaxSpeed = 3;
+    
+    //用户输入的力量 -1  ->   1
+    private float finput = 0.0f;
+
+    //移动的力量放大的倍数
+    public float moveForce = 100;
+
+    //jump的力量放大的倍数
+    public float jumpForce = 550;
+
+    //是否朝右
+    private bool bFaceRight = true;
+    
+    private Transform groundCheck;
+
+    //射线检测结果
+    private bool bGround = false;
+    
+    //是否按下跳跃键
+    private bool bJump = false;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        heroBody = GetComponent<Rigidbody2D>();
+        
+        groundCheck = transform.Find("groundCheck");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        //检测键盘输入  Horizontal，得到 (-1,1)的浮点数
+        finput = Input.GetAxis("Horizontal");
+        
+        //控制转身
+        if (finput < 0 && bFaceRight)
+        {
+            Flip();
+        }
+        else if(finput > 0 && !bFaceRight)
+        {
+            Flip();
+        }
+        
+        //射线检测
+        
+        bGround =  Physics2D.Linecast(transform.position,groundCheck.position,1 << LayerMask.NameToLayer("Ground"));
+        
+        //检测跳跃键
+        bJump = Input.GetButtonDown("Jump");
+
+        Jump();
+    }
+
+    void FixedUpdate()
+    {
+        //Abs绝对值，因为力是向量
+        if (Mathf.Abs(heroBody.velocity.x) < MaxSpeed)
+        {
+            //给物体添加一个力
+            heroBody.AddForce(finput * moveForce * Vector2.right);
+        }
+        //最大速度时
+        if (Mathf.Abs(heroBody.velocity.x) > MaxSpeed)
+        {
+            //设定速度为最大速度
+            heroBody.velocity = 
+                new Vector2(Mathf.Sign(heroBody.velocity.x) *
+                            MaxSpeed, heroBody.velocity.y);
+        }
+    }
+
+    /**
+     * 转身
+     */
+    void Flip()
+    {
+        Vector3 scale = transform.localScale;
+        //反向
+        scale.x *= -1;
+        transform.localScale = scale;
+        bFaceRight = !bFaceRight;
+    }
+
+    /**
+     * Jump
+     */
+    void Jump()
+    {
+        //jump
+        if (bGround && bJump)
+        {
+            heroBody.AddForce(new Vector2(0f, jumpForce ) );
+            bJump = false;
+        }
+        
+    }
+}
