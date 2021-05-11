d15cab85c176068f6de64eaedcaaa9289101f1f1
Xin7K <350943239@qq.com>
2021-05-11 19:17:12 +0800

    完成摄像机跟随和背景补偿

---
+++ MrPotato/Mr.Potato/Assets/Scripts/BackGround.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class BackGround : MonoBehaviour
+{
+    public Transform[] background;
+    public float fParalax = 0.4f;
+    public float layerPraction = 5f;
+    public float fSmooth = 5f;
+
+    private Transform cam;
+
+    //上一帧摄像机位置
+    private Vector3 previousCamPos;
+
+    private Vector3 _vector3;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        cam = Camera.main.transform;
+    }
+
+    private void Start()
+    {
+        previousCamPos = cam.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //每一帧位移偏移量 * fParalax
+        float fParrlaxX = (previousCamPos.x - cam.position.x) * fParalax;
+
+        for (int i = 0; i < background.Length; i++)
+        {
+            float fNewX = background[i].position.x + fParrlaxX * (1 + i * layerPraction);
+
+            Vector3 newPos = new Vector3(fNewX, background[i].position.y,background[i].position.z);
+
+            background[i].position = Vector3.Lerp(background[i].position,
+                newPos,fSmooth * Time.deltaTime
+                );
+        }
+
+        previousCamPos = cam.position;
+    }
+}
---
+++ MrPotato/Mr.Potato/Assets/Scripts/MCamera.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class MCamera : MonoBehaviour
+{
+    private Transform player;
+
+    public float xMargin = 2;
+    public float yMargin = 2;
+
+    public float smoothX = 1;
+    public float smoothY = 1;
+
+    private Vector2 minCamXandY = new Vector2(-8,-8);
+
+    private Vector2 maxCamXandY = new Vector2(8,8);
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    private bool NeedMoveX()
+    {
+        return Mathf.Abs(transform.position.x - player.position.x) > xMargin;
+    }
+    
+    private bool NeedMoveY()
+    {
+        return Mathf.Abs(transform.position.y - player.position.y) > yMargin;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    void FixedUpdate()
+    {
+        TrackPlayer();
+    }
+
+    private void TrackPlayer()
+    {
+        float CamNewX = transform.position.x;
+        float CamNewY = transform.position.y;
+        if (NeedMoveX()) //计算新摄像机位置
+            CamNewX = Mathf.Lerp(CamNewX,player.position.x, smoothX * Time.deltaTime);
+        if (NeedMoveY())
+            CamNewY = Mathf.Lerp(CamNewY, 
+                player.position.y, smoothY * Time.deltaTime);
+        //将新摄像机位置固定在合法范围内
+        CamNewX = Mathf.Clamp(CamNewX, minCamXandY.x, maxCamXandY.x);
+        CamNewY = Mathf.Clamp(CamNewY, minCamXandY.y, maxCamXandY.y);
+        
+        transform.position = new Vector3(CamNewX, CamNewY, 
+            transform.position.z);
+
+    }
+    
+}
